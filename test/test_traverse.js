8b8118d8bf262a5969b01c3d89419ea123081a93
yosuke-furukawa <yosuke.furukawa@gmail.com>
2014-06-30 09:44:04 +0900

    Add traverse

---
+++ test/test_traverse.js
@@ -0,0 +1,23 @@
+var Traverse = require("../lib/traverse");
+var esprima = require("esprima");
+var escodegen = require('escodegen');
+
+var assert = require("power-assert");
+describe('Traverse', function(){
+  describe('#replace()', function(){
+    it('insert function', function(){
+      var src = " function abc() { var test = function(){console.log('hello');}; test();} abc();";
+      var src = " function abc() { var test = function(){console.log('hello');}; test();} abc();";
+      var expected = " function abc() { console.time('abc'); var test = function(){console.time('test'); console.log('hello'); console.timeEnd('test');}; test(); console.timeEnd('abc');} abc();";
+
+      expected = escodegen.generate(esprima.parse(expected));
+      var ast = esprima.parse(src);
+      var enter = "console.time('${name}');";
+      var leave = "console.timeEnd('${name}');";
+      var traverse = new Traverse(ast);
+      var traversedAst = traverse.replace(enter, leave);
+      var code = escodegen.generate(traversedAst);
+      assert.deepEqual(expected, code);
+    });
+  });
+});
